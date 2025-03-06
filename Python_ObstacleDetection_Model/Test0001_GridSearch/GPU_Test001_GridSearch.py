247f4bcf553b89fd8f3ba750d73f6356a4eb9764
jeffersonarpasserini <jefferson.passerini@gmail.com>
2025-03-06 09:13:20 -0300

    test adam relu

--- Python_ObstacleDetection_Model/Test0001_GridSearch/GPU_Test001_GridSearch.py
+++ Python_ObstacleDetection_Model/Test0001_GridSearch/GPU_Test001_GridSearch.py
@@ -414,10 +414,10 @@ if __name__ == "__main__":
 
     # ✅ Definir a grade de hiperparâmetros
     params_grid = {
-        'model__activation': ['relu'],
-        'model__optimizer': ['rmsprop'],
+        'model__activation': ['tanh'],
+        'model__optimizer': ['adam'],
         'model__n_layers': [1, 2, 3],
-        'model__n_neurons': [32,64],
+        'model__n_neurons': [128, 256, 512],
         'model__dropout_rate': [0.0, 0.1, 0.2, 0.3, 0.4, 0.5],
         'model__learning_rate': [0.0005, 0.0001, 0.005, 0.001, 0.05, 0.01, 0.1]
     }
