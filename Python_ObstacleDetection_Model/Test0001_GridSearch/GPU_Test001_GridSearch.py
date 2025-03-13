45525a2afb7fdbc9da1d8b52e5e6505aee29ff21
jeffersonarpasserini <jefferson.passerini@gmail.com>
2025-03-13 09:40:05 -0300

    test relu adam 32 neurons max

--- Python_ObstacleDetection_Model/Test0001_GridSearch/GPU_Test001_GridSearch.py
+++ Python_ObstacleDetection_Model/Test0001_GridSearch/GPU_Test001_GridSearch.py
@@ -27,7 +27,6 @@ from tensorflow.keras.models import Model
 from tensorflow.python.keras.callbacks import ReduceLROnPlateau
 from scikeras.wrappers import KerasClassifier
 
-
 # faz o tf rodar na CPU
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
@@ -419,9 +418,9 @@ if __name__ == "__main__":
     # ✅ Definir a grade de hiperparâmetros
     params_grid = {
         'model__activation': ['relu'],
-        'model__optimizer': ['rmsprop'],
-        'model__n_layers': [1],
-        'model__n_neurons': [256],
+        'model__optimizer': ['adam'],
+        'model__n_layers': [1, 2, 3],
+        'model__n_neurons': [32],
         'model__dropout_rate': [0.0, 0.1, 0.2, 0.3, 0.4, 0.5],
         'model__learning_rate': [0.0005, 0.0001, 0.005, 0.001, 0.05, 0.01, 0.1]
     }
